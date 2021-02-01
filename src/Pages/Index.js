5a4ba541703129d6b229f7e4aff5d70835515128
Igor Magro <igormagro@Igors-MacBook-Pro.local>
2021-02-01 11:55:37 -0300

    Add pagination functionality

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,10 +1,11 @@
 import React, { Fragment, useEffect, useState } from "react";
 import SearchBox from "../Components/SearchBox/SearchBox";
 import BikeCard from "../Components/BikeCard/BikeCard";
+import Pagination from "../Components/Pagination/Pagination";
 import axios from "axios";
 
 const axiosOpt = {
-  url: "https://bikewise.org/api/v2/incidents?proximity=Berlin",
+  url: "https://bikewise.org/api/v2/incidents?per_page=100&proximity=Berlin",
   method: "GET",
 };
 
@@ -16,15 +17,29 @@ const Index = () => {
     fromDate: "",
     toDate: "",
   });
+  const [pagination, setPagination] = useState({
+    currentPage: 1,
+    itemsPerPage: 10,
+    numberOfPages: 0,
+  });
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
   useEffect(() => {
     axios
       .request(axiosOpt)
       .then((res) => {
         setIncidents(res.data.incidents);
         setDisplayedIncidents(res.data.incidents);
+        setPagination((prev) => {
+          return {
+            ...prev,
+            numberOfPages: Math.ceil(res.data.incidents.length / 10),
+          };
+        });
+        setIsLoading(false);
       })
       .catch((err) => {
-        console.log(err);
+        setError(true);
       });
   }, []);
 
@@ -33,7 +48,6 @@ const Index = () => {
     let strToDate = null;
     if (name.split("Date").length > 1) {
       strToDate = new Date(value).getTime() / 1000;
-      console.log(strToDate);
     } else {
       strToDate = value;
     }
@@ -62,15 +76,79 @@ const Index = () => {
       if (toDate) {
         conditions.push("occurred_at <= toDate");
       }
-      console.log(conditions.join("&&"));
       return false || eval(conditions.join("&&"));
     });
     setDisplayedIncidents(newDisplayed);
+    setPagination((prev) => {
+      return { ...prev, numberOfPages: newDisplayed.length / 10 };
+    });
   };
 
-  const displayed = displayedIncidents.filter((el, idx) => {
-    return idx <= 9;
-  });
+  const changePageClickHandler = (e) => {
+    const { name } = e.target;
+    console.log(name);
+    switch (name) {
+      case "prev":
+        setPagination((prev) => {
+          return {
+            ...prev,
+            currentPage: prev.currentPage - 1,
+          };
+        });
+        break;
+      case "next":
+        setPagination((prev) => {
+          return {
+            ...prev,
+            currentPage: prev.currentPage + 1,
+          };
+        });
+        break;
+      case "first":
+        setPagination((prev) => {
+          return {
+            ...prev,
+            currentPage: 1,
+          };
+        });
+        break;
+      case "last":
+        setPagination((prev) => {
+          return {
+            ...prev,
+            currentPage: prev.numberOfPages,
+          };
+        });
+        break;
+
+      default:
+        setPagination((prev) => {
+          return {
+            ...prev,
+            currentPage: +name,
+          };
+        });
+        break;
+    }
+  };
+
+  const displayed = displayedIncidents
+    .filter((el, idx) => {
+      return (
+        (idx - pagination.itemsPerPage * (pagination.currentPage - 1) + 1) *
+          (idx - pagination.itemsPerPage * pagination.currentPage) <
+        0
+      );
+    })
+    .sort((a, b) => {
+      if (a.occurred_at > b.occurred_at) {
+        return -1;
+      }
+      if (a.occurred_at > b.occurred_at) {
+        return 1;
+      }
+      return 0;
+    });
 
   return (
     <Fragment>
@@ -79,18 +157,35 @@ const Index = () => {
         clicked={searchClickHandler}
         changed={iptChangeHandler}
       />
-      {displayed.map((el) => {
-        return (
-          <BikeCard
-            key={el.id}
-            title={el.title}
-            description={el.description}
-            address={el.address}
-            occurred_at={el.occurred_at}
-            imgUrl={el.media.image_url}
+      {error ? (
+        <p style={{ padding: "20px", color: "red" }}>
+          Ooops, something went wrong
+        </p>
+      ) : displayed.length === 0 ? (
+        <p style={{ padding: "20px" }}>
+          {isLoading ? "Loading..." : "No results"}
+        </p>
+      ) : (
+        <Fragment>
+          {displayed.map((el) => {
+            return (
+              <BikeCard
+                key={el.id}
+                title={el.title}
+                description={el.description}
+                address={el.address}
+                occurred_at={el.occurred_at}
+                imgUrl={el.media.image_url}
+              />
+            );
+          })}
+          <Pagination
+            page={pagination.currentPage}
+            clicked={changePageClickHandler}
+            numberOfPages={pagination.numberOfPages}
           />
-        );
-      })}
+        </Fragment>
+      )}
     </Fragment>
   );
 };
